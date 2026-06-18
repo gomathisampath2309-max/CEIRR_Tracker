2cb4bafacf007d767d92db804dbfc05cdf68992f
gomathisampath <gomathisampath2309@gmail.com>
2026-06-18 15:57:23 +0530

    Add files via upload

--- dashboard.py
+++ dashboard.py
@@ -106,7 +106,7 @@ with col2:
 
 with col3:
 
-    st.subheader("PDF(Sat 9 AM Send to All)")
+    st.subheader("PDF(Sat 9 AM Send Mail to All)")
 
     if st.button(
         "Generate PDF",
@@ -121,7 +121,7 @@ st.markdown("---")
 # Complete Process
 # ---------------------------------------------------
 
-st.subheader("Complete Weekly Process (Sat 9 AM Send to All)")
+st.subheader("Complete Weekly Process (Sat 9 AM Mail Send to All)")
 
 if st.button(
     "Run Complete Process",
